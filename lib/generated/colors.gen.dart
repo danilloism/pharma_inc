5bb084fb33a5a351b471afcb0a87d3e0a0b4fd7c
Danillo Ilggner <danilloilggner@gmail.com>
2023-01-08 12:51:13 -0300

    atualização e geração de código de assets

---
+++ lib/generated/colors.gen.dart
@@ -0,0 +1,33 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #FF003461
+  static const Color deepBlue = Color(0xFF003461);
+
+  /// Color: #FFF26522
+  static const Color deepOrange = Color(0xFFF26522);
+
+  /// Color: #FF00AFAD
+  static const Color seaBlue = Color(0xFF00AFAD);
+
+  /// Color: #FF0083CA
+  static const Color softBlue = Color(0xFF0083CA);
+
+  /// Color: #FFFCAF17
+  static const Color softOrange = Color(0xFFFCAF17);
+
+  /// Color: #ffffffff
+  static const Color white = Color(0xFFFFFFFF);
+}
